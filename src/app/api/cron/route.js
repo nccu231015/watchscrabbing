2760ee5f2f3674f26e20496444642a673dc9815e
nccu231015 <istudiodesign.tw@gmail.com>
2024-07-11 15:03:54 +0800

    edit cron jobs

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -3,14 +3,13 @@ import { NextResponse } from "next/server";
 
 import cron from 'node-cron'
 
+export dynamic = "force-dynamic"
+export maxDuration = 600
+export const revalidate = 0
 export async function POST(req, res) {
 
     try {
-
-
         main();
-
-
         return NextResponse.json({ data: 'Success', status: 200 });
 
     } catch (error) {
