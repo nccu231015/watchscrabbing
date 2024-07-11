40be9b1482ce1823f9141ab0415cb3372058eeb5
nccu231015 <istudiodesign.tw@gmail.com>
2024-07-11 15:09:11 +0800

    cron update3

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 
 import cron from 'node-cron'
 
-export const dynamic = "force-dynamic"
-export const  maxDuration = 600
-export const revalidate = 0
+export const maxDuration = 600; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function POST(req, res) {
 
