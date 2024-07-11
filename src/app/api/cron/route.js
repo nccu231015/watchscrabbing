4059fdb11235590e10fc826e12be8d3ae2fa3de6
nccu231015 <istudiodesign.tw@gmail.com>
2024-07-11 16:21:04 +0800

    add

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -4,26 +4,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const AUTH_USER = process.env.CRON_AUTH_USER;
-const AUTH_PASS = process.env.CRON_AUTH_PASS;
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
 
 export async function GET(req) {
-    const auth = req.query.auth;
 
-    if (!auth) {
-        return new NextResponse('Unauthorized with no auth', { status: 401 });
-    }
-
-    const decodedCredentials = atob(auth);
-    const [user, pass] = decodedCredentials.split(':');
-
-    if (user !== AUTH_USER || pass !== AUTH_PASS) {
-        return new NextResponse('Unauthorized from Daniel', { status: 401 });
-    }
-
+    if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+        return res.status(401).end('Unauthorized');
+      }
+      
     try {
         await main();
         return NextResponse.json({ data: 'Success', status: 200 });
