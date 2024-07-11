ed4963329fabbd5812e10c45cd1308f5b0cab024
nccu231015 <istudiodesign.tw@gmail.com>
2024-07-11 15:35:54 +0800

    change GET

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -3,17 +3,12 @@ import { NextResponse } from "next/server";
 
 import cron from 'node-cron'
 
-export const maxDuration = 60;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export async function POST(req, res) {
+export async function GET(req) {
 
-    
     try {
-        if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-            return res.status(401).end('Unauthorized');
-          }
         main();
         return NextResponse.json({ data: 'Success', status: 200 });
 
