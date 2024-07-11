9be6ec571610dc481710fd7a8dd98e7c616c2aa5
nccu231015 <istudiodesign.tw@gmail.com>
2024-07-11 15:51:27 +0800

    edit authentication

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -1,20 +1,37 @@
 import main from "@/lib/main";
 import { NextResponse } from "next/server";
 
-import cron from 'node-cron'
+const AUTH_USER = process.env.CRON_AUTH_USER;
+const AUTH_PASS = process.env.CRON_AUTH_PASS;
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export async function GET(req) {
+    const authHeader = req.headers.get('authorization');
 
-    try {
-        main();
-        return NextResponse.json({ data: 'Success', status: 200 });
-
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({ error: error }, { status: 500 })
+    if (!authHeader) {
+        return new NextResponse('Unauthorized', { status: 401 });
     }
 
+    const [type, credentials] = authHeader.split(' ');
+
+    if (type !== 'Basic') {
+        return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const decodedCredentials = atob(credentials);
+    const [user, pass] = decodedCredentials.split(':');
+
+    if (user !== AUTH_USER || pass !== AUTH_PASS) {
+        return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    try {
+        await main();
+        return NextResponse.json({ data: 'Success', status: 200 });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
 }
