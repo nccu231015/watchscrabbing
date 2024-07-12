d3a29a6afe2f280807e4f9b7229e5e6e1ae2a6eb
nccu231015 <istudiodesign.tw@gmail.com>
2024-07-12 23:08:35 +0800

    change route.js

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -1,10 +1,12 @@
-import main from "@/lib/main";
+import main, { getpages } from "@/lib/main";
 import { NextResponse } from "next/server";
 import dotenv from 'dotenv';
+import fetchWatchMiddleware from "@/lib/Database/fetchWatch";
 
 dotenv.config();
 
 export const maxDuration = 60;
+export const dynamic = "force-dynamic"
 
 export async function GET(req) {
 
@@ -14,7 +16,8 @@ export async function GET(req) {
 
     try {
         const shop = req.headers.get('shop')
-        await main(shop);
+        await fetchWatchMiddleware();
+        await getpages(shop);
         return NextResponse.json({ data: 'Success', status: 200 });
     } catch (error) {
         console.error(error);
