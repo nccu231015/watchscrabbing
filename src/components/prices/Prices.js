fc00c3e7f182aada1245828398e4dc678a13f22b
nccu231015 <istudiodesign.tw@gmail.com>
2025-02-12 12:02:48 +0800

    changing logic

--- src/components/prices/Prices.js
+++ src/components/prices/Prices.js
@@ -2,30 +2,31 @@ import "../card/card.css"
 import PriceTags from "./priceTag"
 import moment from "moment/moment";
 
-
-   
-
 export default function Prices({price, sold, lastupdate}){
-  console.log(price)
-    return (
-      
-        <div className="price">
-        {sold?<PriceTags time={lastupdate} check="2" price="sold"/>:<></>}
-    
-    {price.map((priceItem, i) => {
-        const check = i === price.length-1 ? 0 : priceItem.price < price[i + 1].price ? -1 : 1;
+  // 根據 updatedAt 進行排序，新的在上面
+  const sortedPrices = [...price].sort((a, b) => {
+    const timeA = moment(a.updatedAt);
+    const timeB = moment(b.updatedAt);
+    return timeB - timeA;  // 降序排列
+  });
+
+  return (
+    <div className="price">
+      {sold ? <PriceTags time={lastupdate} check="2" price="sold"/> : <></>}
+  
+      {sortedPrices.map((priceItem, i) => {
+        const check = i === sortedPrices.length-1 ? 0 : 
+                     priceItem.price < sortedPrices[i + 1].price ? -1 : 1;
         
         return (
           <PriceTags
-            key={i} // It's a good practice to use a unique key for list items
+            key={i}
             price={priceItem.price}
             time={priceItem.updatedAt}
             check={check}
           />
         );
       })}
-      
-        </div>
-       
-    )
+    </div>
+  )
 }
