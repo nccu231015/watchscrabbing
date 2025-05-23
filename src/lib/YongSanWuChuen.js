b657a0d79ff54a041e03dec8b2af738af5ad368a
nccu231015 <istudiodesign.tw@gmail.com>
2025-05-23 16:39:54 +0800

    update 永生
    update 永生

--- src/lib/YongSanWuChuen.js
+++ src/lib/YongSanWuChuen.js
@@ -28,7 +28,7 @@ export const YSWC_count = async ()=>{
     await page.goto(url_YSWC(1),{waitUntil:'networkidle0'});
     const pages = await page.evaluate(()=>{
      
-        const pgs = document.querySelector("div.sc-16fedlx-0.bKdNcg.sc-5ehcvq-5.fuQJOD > div.sc-16fedlx-1.jnNtwX > a:nth-last-child(1)")
+        const pgs = document.querySelector("div.sc-16fedlx-1.jXVogd > a:nth-last-child(1)")
         return pgs.innerText
     })
 
